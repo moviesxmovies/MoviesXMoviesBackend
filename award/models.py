c95df59ff777f5dc024f0ad8a4a8d36159c5047f
rabgonzalez <rubalba.rag@gmail.com>
2026-01-30 19:40:10 +0000

    feat: award model finished, issue #2 closed

---
+++ award/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Award(models.Model):
+    class Category(models.TextChoices):
+        # MOVIES
+        BEST_PICTURE = 'BP', 'Best Picture'
+        BEST_ANIMATED_FEATURE = 'BA', 'Best Animated Feature'
+        BEST_DOCUMENTARY = 'BD', 'Best Documentary'
+        BEST_INTERNATIONAL_FEATURE = 'IF', 'Best International Feature'
+        
+        # ACTORS
+        BEST_ACTOR = 'MA', 'Best Actor in a Leading Role'
+        BEST_ACTRESS = 'WA', 'Best Actress in a Leading Role'
+        BEST_SUPPORTING_ACTOR = 'SA', 'Best Supporting Actor'
+        BEST_SUPPORTING_ACTRESS = 'SX', 'Best Supporting Actress'
+        
+        # DIRECTING
+        BEST_DIRECTOR = 'DR', 'Best Director'
+        BEST_DEBUT_DIRECTOR = 'DD', 'Best Debut Director'
+        
+    name = models.CharField(max_length=32, unique=True)
+    slug = models.CharField(max_length=32, unique=True)
+    category = models.CharField(choices=Category, blank=True)
+    icon = models.ImageField(upload_to='awards', default='awards/no-award.png')
+    date = models.DateField(blank=True, null=True)
--- main/settings.py
+++ main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'award.apps.AwardConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+
