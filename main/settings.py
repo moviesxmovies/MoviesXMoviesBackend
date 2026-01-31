9da7446d203642e27661bf1ad20f356506455d9a
JonayKB <jonaykb@gmail.com>
2026-01-31 21:35:20 +0000

    ci: changed commands

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='A_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=config.boolean)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: v.split(','), default='*')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=config.list, default='*')
 
 
 # Application definition
