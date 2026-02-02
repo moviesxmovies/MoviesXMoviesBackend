134240f098eb4d3d3bdeac3d199473b4ed626ddc
JonayKB <jonaykb@gmail.com>
2026-02-02 13:57:44 +0000

    docs: Added class documentation

--- persons/models.py
+++ persons/models.py
@@ -4,7 +4,19 @@ from shared.models import Timestamps
 
 
 class Person(Timestamps):
+    """
+    Model representing a celebrity, usually an actor/actress or director.
+
+    Attributes:
+        name (CharField): The full name of the person.
+        slug (SlugField): A URL-friendly version of the person's name.
+        image (ImageField): A profile image of the person.
+        country (CharField): The country of origin of the person.
+    """
     class Country(models.TextChoices):
+        """
+        Enumeration of supported countries for persons.
+        """
         SPAIN = 'ES', 'Spain'
         ENGLAND = 'EN', 'England'
 
--- shared/models.py
+++ shared/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 
 class Timestamps(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    'created_at' and 'updated_at' fields to any model that inherits from it.
+
+    Attributes:
+        created_at (DateTimeField): The date and time when the record was created.
+        updated_at (DateTimeField): The date and time when the record was last updated.
+
+    Note:
+        This is an abstract model and will not create a database table.
+
+    """
+
     class Meta:
-        abstract= True
+        abstract = True
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
