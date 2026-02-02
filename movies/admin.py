5e848e36034eb372577ec847cc9923f84a086968
rabgonzalez <rubalba.rag@gmail.com>
2026-02-02 20:07:57 +0000

    feat: review model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "synopsis", "release_date", "cover"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("directors", "actors", "genres", "platforms")
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "isPositive"]
+    raw_id_fields = ("user", "movie")
