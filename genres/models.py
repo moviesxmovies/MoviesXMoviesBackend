913f38e3132c594ff20612894788e2bc20295829
JonayKB <jonaykb@gmail.com>
2026-02-02 14:45:06 +0000

    docs: Added classes to docs

--- genres/models.py
+++ genres/models.py
@@ -3,5 +3,11 @@ from django.db import models
 from shared.models import Timestamps
 
 class Genre(Timestamps):
+    """
+    A movie genre model
+    Attributes:
+        name (models.CharField): The name of the genre.
+        slug (models.SlugField): A URL-friendly version of the genre's name.
+    """
     name = models.CharField(max_length=32, unique=True)
     slug = models.SlugField(max_length=32, unique=True)
