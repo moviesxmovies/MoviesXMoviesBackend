85b1b7f1cdc0e7da0dc101ddeeb67edbe8ed6909
rabgonzalez <rubalba.rag@gmail.com>
2026-02-04 12:24:36 +0000

    fix: oauth login method fixed

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
 from django.urls import path, include
 from shared.views import GoogleLogin
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-=======
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from shared.views import CustomTokenObtainPairView
->>>>>>> ee8bc6f2f600804fc880492b7f502799d004db88
 
 urlpatterns = [
     path(
