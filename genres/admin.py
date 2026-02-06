25bf17c864d7006d41ecc043d098e8fe380e84a7
rabgonzalez <rubalba.rag@gmail.com>
2026-02-06 15:57:49 +0000

    refactor: some fixes in genre admin and platform admin refactored

--- genres/admin.py
+++ genres/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+
 from .models import Genre
+
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ["name"]
-    search_fields = ["name", "slug"]
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ['name']
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+    exclude = ['deleted_at']
