25ace2aa5bb9500c4dd40cbfdd7f8d32429dfa3e
rabgonzalez <rubalba.rag@gmail.com>
2026-02-06 16:09:57 +0000

    refactor: person admin refactored

--- persons/admin.py
+++ persons/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'country']
+    search_fields = ['name', 'country']
+    prepopulated_fields = {'slug': ('name',)}
+    exclude = ['deleted_at']
