95772a5359e5cd0171306d8082cdd26690a18652
rabgonzalez <rubalba.rag@gmail.com>
2026-02-06 16:28:39 +0000

    refactor: award admin refactored

--- awards/admin.py
+++ awards/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+
 from .models import Award
+
+
 @admin.register(Award)
 class AwardAdmin(admin.ModelAdmin):
-    list_display = ["name", "slug", "category", "icon", "date", "created_at"]
-    prepopulated_fields = {"slug": ("name",)}
+    list_display = ['name', 'category', 'date']
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+    exclude = ['deleted_at']
+    list_filter = ['category']
