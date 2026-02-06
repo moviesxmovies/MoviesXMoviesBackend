76f62297824d6954b9874c882d9952a5ae029202
rabgonzalez <rubalba.rag@gmail.com>
2026-02-06 15:34:09 +0000

    refactor: genre admin refactored

--- genres/admin.py
+++ genres/admin.py
@@ -3,5 +3,6 @@ from .models import Genre
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ["name", "slug", "created_at"]
+    list_display = ["name"]
+    search_fields = ["name", "slug"]
     prepopulated_fields = {"slug": ("name",)}
