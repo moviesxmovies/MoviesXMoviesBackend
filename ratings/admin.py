548cbf4e47324f6339cf47132a50c215d330ef50
rabgonzalez <rubalba.rag@gmail.com>
2026-02-06 18:23:17 +0000

    refactor: rating admin refactored (movie admin remaining)

--- ratings/admin.py
+++ ratings/admin.py
@@ -3,5 +3,7 @@ from .models import Rating
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ["rating", "user", "movie"]
-    raw_id_fields = ("user", "movie")
+    list_display = ['rating', 'user', 'movie']
+    exclude = ['deleted_at']
+    list_filter = ['rating']
+    autocomplete_fields = ['user']
