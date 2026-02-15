964c7a4cd254e4dce3566fb544479d9e76071b68
JonayKB <jonaykb@gmail.com>
2026-02-15 13:05:21 +0000

    fix: changed rq host on deployment

--- main/settings.py
+++ main/settings.py
@@ -91,7 +91,7 @@ MIDDLEWARE = [
 ]
 RQ_QUEUES = {
     'default': {
-        'HOST': 'localhost',
+        'HOST': 'localhost' if config('DEBUG', default=True, cast=config.boolean) else 'redis',
         'PORT': 6379,
         'DB': 0,
     },
