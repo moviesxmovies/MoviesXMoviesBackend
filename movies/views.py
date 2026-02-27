4c4911f278040d56e1f860187c0a0d08b8009b56
JonayKB <jonaykb@gmail.com>
2026-02-27 20:18:45 +0000

    feat: created wrapper and save movie

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,23 @@
-from django.core.paginator import Paginator
-from django.http import JsonResponse
-from drf_spectacular.utils import OpenApiParameter, extend_schema
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework.decorators import api_view
 
 from movies.models import Movie
 from movies.serializers import MovieSerializer
 from ratings.serializers import RatingSerializer
+from reviews.models import Review
 from reviews.serializers import ReviewSerializer
-from shared.decorators import get_query_params, require_http_methods
+from shared.decorators import get_body, get_query_params, require_http_methods
 from shared.utils import get_paginated_response
 from users.decorators import auth_required
+from rest_framework import serializers
+
+
+class ReviewSaveSerializer(serializers.Serializer):
+    is_positive = serializers.BooleanField(
+        required=True, help_text='If the serializer is positive or not'
+    )
+    title = serializers.CharField(required=True, help_text='Title of review')
+    content = serializers.CharField(required=True, help_text='Content of review')
 
 
 @api_view(['GET'])
@@ -27,16 +35,6 @@ def movie_detail(request, movie: Movie):
         OpenApiParameter(name='limit', description='Items per page', required=False, type=int),
     ],
 )
-@api_view(['GET'])
-@require_http_methods(['GET'])
-@auth_required
-@get_query_params('page', 'limit')
-def movie_reviews(request, movie: Movie, page: int = 1, limit: int = 10):
-    return get_paginated_response(
-        movie.reviews.all().order_by('-created_at'), ReviewSerializer, request, page, limit
-    )
-
-
 @extend_schema(
     responses={200: None, 400: None, 404: None},
     description='Get ratings of friends for a specific movie',
@@ -50,5 +48,51 @@ def movie_reviews(request, movie: Movie, page: int = 1, limit: int = 10):
 @auth_required
 @get_query_params('page', 'limit')
 def movie_friends_ratings(request, movie: Movie, page: int = 1, limit: int = 10):
-    ratings_query = movie.ratings.filter(user__in=request.user.friends.all()).order_by('-created_at')
+    ratings_query = movie.ratings.filter(user__in=request.user.friends.all()).order_by(
+        '-created_at'
+    )
     return get_paginated_response(ratings_query, RatingSerializer, request, page, limit)
+
+
+# REVIEWS
+@extend_schema(
+    methods=['GET'],
+    description='Get reviews specific movie',
+    parameters=[
+        OpenApiParameter(name='page', description='Page number', required=False, type=int),
+        OpenApiParameter(name='limit', description='Items per page', required=False, type=int),
+    ],
+    operation_id='get_movie_reviews',
+)
+@extend_schema(
+    methods=['POST'],
+    description='Create Review of a movie',
+    responses={201: ReviewSerializer},
+    request=ReviewSaveSerializer,
+    operation_id='create_movie_review',
+)
+@api_view(['GET', 'POST'])
+@auth_required
+def movie_review_wrapper(request, movie: Movie):
+    match request.method:
+        case 'POST':
+            save_movie(request, movie)
+        case 'GET':
+            movie_reviews(request, movie)
+
+
+@require_http_methods(['GET'])
+@get_query_params('page', 'limit')
+def movie_reviews(request, movie: Movie, page: int = 1, limit: int = 10):
+    return get_paginated_response(
+        movie.reviews.all().order_by('-created_at'), ReviewSerializer, request, page, limit
+    )
+
+
+@require_http_methods(['POST'])
+@get_body(Review, ['is_positive', 'title', 'content'])
+def save_movie(request, movie: Movie, review: Review):
+    review.user = request.user
+    review.movie = movie
+    review.save()
+    return ReviewSerializer(review, request=request)
