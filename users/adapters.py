e8d77b590db3449dd004eb619ab95823d5910a8b
JonayKB <jonaykb@gmail.com>
2026-03-05 22:21:12 +0000

    feat: added donwlioand image

--- users/adapters.py
+++ users/adapters.py
@@ -1,6 +1,11 @@
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from io import BytesIO
 
-from users.models import User
+import requests
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth import get_user_model
+from django.core.files import File
+
+User = get_user_model()
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
@@ -19,8 +24,13 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
         data = sociallogin.account.extra_data
-        picture = data.get('picture')
-        if picture:
-            user.picture = picture
-            user.save()
+        picture_url = data.get('picture')
+        if picture_url and not user.picture:
+            try:
+                response = requests.get(picture_url, timeout=5)
+                if response.status_code == 200:
+                    filename = f'profile_{user.pk}.jpg'
+                    user.picture.save(filename, File(BytesIO(response.content)), save=True)
+            except requests.RequestException:
+                pass
         return user
