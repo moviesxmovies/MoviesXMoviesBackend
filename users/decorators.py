dbe1fdd389405d8df15340163796d1fc69759f46
JonayKB <jonaykb@gmail.com>
2026-04-20 09:06:02 +0100

    refactor: refactores auth_requiered

--- users/decorators.py
+++ users/decorators.py
@@ -29,6 +29,29 @@ def auth_required(require_verification=True):
         before calling the original view.
     """
 
+    def __check_token(auth_header, request):
+        token = auth_header.split(' ')[1]
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        if payload.get('token_type', '') != ACCESS_TYPE:
+            return JsonResponse(
+                {'error': _('Token type is invalid')}, status=HTTPStatus.BAD_REQUEST
+            )
+        try:
+            request.user = User.objects.get(pk=payload.get('user_id'))
+        except User.DoesNotExist:
+            return JsonResponse({'error': _('Token is invalid')}, status=HTTPStatus.UNAUTHORIZED)
+
+    def __check_verification(request):
+        if (
+            not request.user.verified
+            and request.path not in [reverse('verify_user'), reverse('resend_verification_email')]
+            and require_verification
+        ):
+            return JsonResponse(
+                {'error': _('User account is not verified')},
+                status=HTTPStatus.UNAUTHORIZED,
+            )
+
     def decorator(func):
 
         def wrapper(request, *args, **kwargs):
@@ -49,31 +72,13 @@ def auth_required(require_verification=True):
             auth_header = request.headers.get('Authorization', '')
             try:
                 if auth_header.startswith('Bearer '):
-                    token = auth_header.split(' ')[1]
-                    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-                    if payload.get('token_type', '') != ACCESS_TYPE:
-                        return JsonResponse(
-                            {'error': _('Token type is invalid')}, status=HTTPStatus.BAD_REQUEST
-                        )
-                    try:
-                        request.user = User.objects.get(pk=payload.get('user_id'))
-                    except User.DoesNotExist:
-                        return JsonResponse(
-                            {'error': _('Token is invalid')}, status=HTTPStatus.UNAUTHORIZED
-                        )
-                    if (
-                        not request.user.verified
-                        and request.path
-                        not in [
-                            reverse('verify_user'),
-                            reverse('resend_verification_email'),
-                        ]
-                        and require_verification
-                    ):
-                        return JsonResponse(
-                            {'error': _('User account is not verified')},
-                            status=HTTPStatus.UNAUTHORIZED,
-                        )
+                    token_response = __check_token(auth_header, request)
+                    if token_response:
+                        return token_response
+
+                    verification_response = __check_verification(request)
+                    if verification_response:
+                        return verification_response
                     return func(request, *args, **kwargs)
                 return JsonResponse(
                     {'error': _('You need to be authenticated')}, status=HTTPStatus.UNAUTHORIZED
